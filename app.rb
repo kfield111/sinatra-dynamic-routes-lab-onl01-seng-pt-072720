ce8dde5ad144ecaea6e8bd7ebdb8a16d85bcb1d4
Kelin Field <kfield111@hotmail.com>
2020-10-25 15:51:29 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -28,9 +28,16 @@ class App < Sinatra::Base
     @op = params[:operation].gsub(/\A"|"\z/,’’)
     @num2 = params[:number2].to_i
 
-    "#{num1}, #{op}, #{num2}."
-    binding.pry
-
+    case @op
+      when "add"
+        (@num1 + @num2).to_s
+      when "subtract"
+        (@num1 - @num2).to_s
+      when "multiply"
+        (@num1 * @num2).to_s
+      when "divide"
+        (@num1 / @num2).to_s
+      end
+    end
   end
-
 end
