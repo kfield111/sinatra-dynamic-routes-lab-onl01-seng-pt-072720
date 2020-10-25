0969ae66e6f2de7da686583a1154de9b29d10420
Kelin Field <kfield111@hotmail.com>
2020-10-25 15:11:29 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -11,12 +11,12 @@ class App < Sinatra::Base
     "#{params[:number].to_i * params[:number].to_i}"
   end
 
-  get 'say/:number/:phrase' do
+  get '/say/:number/:phrase' do
     times_to_multiply = params[:number].to_i
     times_to_multiply.times {puts "#{params[:phrase]}"}
   end
 
-  get 'say/:word1/:word2/:word3/:word4/:word5' do
+  get '/say/:word1/:word2/:word3/:word4/:word5' do
     "#{params[:word1] + params[:word2] + params[:word3] + params[:word4] + params[:word5]}"
   end
 
