63e0600d9097b2021bc7191b12716df1f2be92f8
Kelin Field <kfield111@hotmail.com>
2020-10-25 15:43:01 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -26,7 +26,7 @@ class App < Sinatra::Base
   get '/:operation/:number1/:number2' do
     @num1 = params[:number1].to_i
     @op = params[:operation].gsub(/\A"|"\z/,’’)
-    @num2 = params[:number2].to_i}
+    @num2 = params[:number2].to_i
 
     result = @num1 + @op + @num2
 
