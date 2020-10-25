5cb797f9e580070fc4933496dcb10958ca84d7b6
Kelin Field <kfield111@hotmail.com>
2020-10-25 15:40:14 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -24,7 +24,7 @@ class App < Sinatra::Base
   end
 
   get '/:operation/:number1/:number2' do
-    result = params[:number1].to_i + params[:operation] + params[:number2].to_i
+    result = params[:number1].to_i params[:operation].gsub(/\A"|"\z/,’’) params[:number2].to_i
     result.to_s
   end
 
