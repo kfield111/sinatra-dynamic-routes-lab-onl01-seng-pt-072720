1d0b88edcca29b6cd0f281b72d8d47c136ab5fff
Kelin Field <kfield111@hotmail.com>
2020-10-25 15:06:30 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -20,7 +20,7 @@ class App < Sinatra::Base
   end
 
   get '/:operation/:number1/:number2' do
-    result = params[number1] params[operation] params[number2]
+    result = params[number1].to_i params[operation] params[number2].to_i
     result.to_s
   end
 
